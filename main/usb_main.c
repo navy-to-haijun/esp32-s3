7a71a8b1ffeb4e34d10fd83a2e9f123380541e74
haijun <linux@haijun.com>
2024-09-17 14:51:53 +0800

    移植cherryUSB

--- main/usb_main.c
+++ main/usb_main.c
@@ -8,17 +8,19 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 
+#include "sdkconfig.h"
+#include "usbd_core.h"
+
+
 static const char *TAG = "main-task";
-
-
-
+void cdc_acm_init1(uint8_t busid, uintptr_t reg_base);
 void app_main(void)
 {
-   
+    ESP_LOGI(TAG, "Hello world!");
+   cdc_acm_init1(0, 0x60080000);
     
    while (1)
    {
-        ESP_LOGI(TAG, "Hello world!");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(10 / portTICK_PERIOD_MS);
    }
 }
